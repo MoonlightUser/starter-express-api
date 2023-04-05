9f2b1fdf643b2e659c4bad1a30a001065db0e9a2
Tymur <tymurmarenych@gmail.com>
2023-04-05 03:23:58 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,95 +1,107 @@
-const express = require('express');
-const app = express();
-// const cors = require('cors');
-// const fetch = require('node-fetch');
-const clientURL = "http://ivy.and.tymurblog.com"
-// const clientURL = "http://127.0.0.1:5501" //for testing
-const getPixelsAPI = "/getPixels.php"
-const safePixelAPI = "/insertPixels.php"
+const cors = require('cors');
+const express = require('express')
+const app = express()
+const CLIENT_URL = 'http://127.0.0.1:5500' // test
 
-app.use(express.static('public'));
-// app.use(cors({
-//     origin: clientURL,
-// }));
 
-app.post('/my-sql-get', (req, res) => {
-  console.log("I was triggered to get data");
-  getPixels()
-    .then(response => {
-      response = JSON.stringify(response);
-      console.log(response);
+app.use(cors({
+    origin: CLIENT_URL,
+}));
+app.all('/', (req, res) => {
+    console.log("Just got a request!")
+    res.send('Yo!')
+})
+app.listen(process.env.PORT || 3000)
 
-        return res.send(response);
-      });   
-});
 
-app.post('/', (req, res) => {
-  console.log("server is running");
-  res.send('Yo!')
-});
+// const clientURL = "http://ivy.and.tymurblog.com"
+// // const clientURL = "http://127.0.0.1:5501" //for testing
+// const getPixelsAPI = "/getPixels.php"
+// const safePixelAPI = "/insertPixels.php"
 
-// app.post('/my-sql-post', (req, res) => {
-//   console.log("I was triggered to post data");
-//   safePixel(res)
-// .then(response => {
-//     console.log(response);
-//   });   
+// app.use(express.static('public'));
+// // app.use(cors({
+// //     origin: clientURL,
+// // }));
+
+// app.post('/my-sql-get', (req, res) => {
+//   console.log("I was triggered to get data");
+//   getPixels()
+//     .then(response => {
+//       response = JSON.stringify(response);
+//       console.log(response);
+
+//         return res.send(response);
+//       });   
 // });
 
+// app.post('/', (req, res) => {
+//   console.log("server is running");
+//   res.send('Yo!')
+// });
+
+// // app.post('/my-sql-post', (req, res) => {
+// //   console.log("I was triggered to post data");
+// //   safePixel(res)
+// // .then(response => {
+// //     console.log(response);
+// //   });   
+// // });
 
 
-async function getPixels() {
-  try {
-    const response = await fetch(clientURL+getPixelsAPI);
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error('Error:', error);
-  }
-}
 
-// async function safePixel(x, y, color) {
-//   const pixel = "x=" + x + "&y=" + y + "&color=" + color;
-//   console.log(pixel);
+// async function getPixels() {
 //   try {
-//       const response = await fetch("http://tymurblog.com/chess/insertPixels.php?" + pixel);
-//       const data = await response.json();
-//       console.log((data));
-//       return data;
+//     const response = await fetch(clientURL+getPixelsAPI);
+//     const data = await response.json();
+//     return data;
 //   } catch (error) {
-//       console.error('Error:', error);
+//     console.error('Error:', error);
 //   }
 // }
 
+// // async function safePixel(x, y, color) {
+// //   const pixel = "x=" + x + "&y=" + y + "&color=" + color;
+// //   console.log(pixel);
+// //   try {
+// //       const response = await fetch("http://tymurblog.com/chess/insertPixels.php?" + pixel);
+// //       const data = await response.json();
+// //       console.log((data));
+// //       return data;
+// //   } catch (error) {
+// //       console.error('Error:', error);
+// //   }
+// // }
 
-// app.post('/trigger-event', (req, res) => {
-// console.log("I was triggered to change color")
-//   const { color } = req.body;
-//   pusher.trigger('my-chess', 'change-color', { color });
-//   res.sendStatus(200);
-// });
 
-// app.post('/clear', (req, res) => {
-//   console.log("I was triggered to clear")
-//   pusher.trigger('my-chess', 'clear', {});
-//   res.sendStatus(200);
-// })
+// // app.post('/trigger-event', (req, res) => {
+// // console.log("I was triggered to change color")
+// //   const { color } = req.body;
+// //   pusher.trigger('my-chess', 'change-color', { color });
+// //   res.sendStatus(200);
+// // });
 
-// app.post('/draw', (req, res) => {
-//   console.log("I was triggered to draw")
-//   const x = req.body.x;
-//   const y = req.body.y;
-//   const color = req.body.color;
-//   console.log(x, y, color);
-//   safePixel(x, y, color)
-//   pusher.trigger('my-chess', 'put-pixel', { x, y, color });
-//   res.sendStatus(200);
-// });
+// // app.post('/clear', (req, res) => {
+// //   console.log("I was triggered to clear")
+// //   pusher.trigger('my-chess', 'clear', {});
+// //   res.sendStatus(200);
+// // })
 
-// var pusher = new Pusher({
-//   appId: '1563257',
-//   key: '37167ed4b28138b6fe32',
-//   secret: '658bc35f8bdb05da7595',
-//   cluster: 'eu',
-// });
+// // app.post('/draw', (req, res) => {
+// //   console.log("I was triggered to draw")
+// //   const x = req.body.x;
+// //   const y = req.body.y;
+// //   const color = req.body.color;
+// //   console.log(x, y, color);
+// //   safePixel(x, y, color)
+// //   pusher.trigger('my-chess', 'put-pixel', { x, y, color });
+// //   res.sendStatus(200);
+// // });
+
+// // var pusher = new Pusher({
+// //   appId: '1563257',
+// //   key: '37167ed4b28138b6fe32',
+// //   secret: '658bc35f8bdb05da7595',
+// //   cluster: 'eu',
+// // });
 

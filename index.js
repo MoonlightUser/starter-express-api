8457bce45f77655b8218b0d1fce852192f24eff6
Tymur <tymurmarenych@gmail.com>
2023-04-12 11:23:38 +0100

    Update index.js

--- index.js
+++ index.js
@@ -38,12 +38,31 @@ app.use(bodyParser.json())
 //     origin: CLIENT_URL,
 // }));
 
-app.use(cors({
-    "origin": CLIENT_URL,
-    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
-    "preflightContinue": false,
-    "optionsSuccessStatus": 204
-  }))
+// app.use(cors({
+//     "origin": CLIENT_URL,
+//     "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+//     "preflightContinue": false,
+//     "optionsSuccessStatus": 204
+//   }))
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', CLIENT_URL);
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 app.all('/', (req, res) => {
     console.log("Just got a request!")  
     res.send('Yo!')
